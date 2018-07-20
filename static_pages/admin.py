dc97a9a48cbe90691d531e2bd35ea9a74a8e051e
Sean Carey <seancarey18@gmail.com>
2018-07-20 10:46:47 -0400

    Adds Micropost model

--- static_pages/admin.py
+++ static_pages/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Micropost
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
 	exclude = ('groups',)
+
+@admin.register(Micropost)
+class MicropostAdmin(admin.ModelAdmin):
+	pass
--- static_pages/models.py
+++ static_pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.safestring import mark_safe
 from django.contrib.auth.models import AbstractUser
 
@@ -12,6 +13,9 @@ class User(AbstractUser):
 
     REQUIRED_FIELDS = ['email']
 
+    def __str__(self):
+        return self.username
+
     def gravatar(self, size=60):
         digest = hashlib.md5(self.email.encode()).hexdigest()
         url = f'http://www.gravatar.com/avatar/{digest}'
@@ -22,3 +26,21 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse('static_pages:profile', args=(self.pk,))
+
+
+class Micropost(models.Model):
+    content = models.TextField(max_length=140)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+
+    def __str__(self):
+        content = self.content.split(' ')
+        return ' '.join(content[:10]) + '...' if len(content) > 10\
+            else ' '.join(content)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.created_at = timezone.now()
+        self.updated_at = timezone.now()
+        return super().save(*args, **kwargs)
